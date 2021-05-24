59931bd78787e9875a78f15c1627c654277ed199
木鸢 <hnx.lemon@gmail.com>
2021-05-24 19:25:04 +0800

    docs: 迁移 code format 文章 (ing...)

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,36 +1,36 @@
 // .vuepress/config.js
 module.exports = {
-  base: '/ii/',
-  title: '木鸢', // Title for the site. This will be displayed in the navbar.
+  base: "/ii/",
+  title: "木鸢", // Title for the site. This will be displayed in the navbar.
   description: "Web development, Frontend, JavaScript, TypeScript",
-  theme: '@vuepress/theme-blog',
+  theme: "@vuepress/theme-blog",
   themeConfig: {
     smoothScroll: true,
     // Please keep looking down to see the available options.
     directories: [
       {
-        id: '_essays',
-        dirname: '_essays',
-        title: '随笔',
-        path: '/essays/',
-        itemPermalink: '/essays/:year/:month/:day/:slug',
+        id: "_essays",
+        dirname: "_essays",
+        title: "随笔",
+        path: "/essays/",
+        itemPermalink: "/essays/:year/:month/:day/:slug",
       },
       {
-        id: '_programming',
-        dirname: '_programming',
-        title: '编程',
-        path: '/programming/',
-        itemPermalink: '/programming/:year/:month/:day/:slug',
+        id: "_programming",
+        dirname: "_programming",
+        title: "编程",
+        path: "/programming/",
+        itemPermalink: "/programming/:year/:month/:day/:slug",
       },
     ],
     nav: [
       {
-        text: '编程',
-        link: '/programming/',
+        text: "编程",
+        link: "/programming/",
       },
       {
-        text: '随笔',
-        link: '/essays/',
+        text: "随笔",
+        link: "/essays/",
       },
       // {
       //   text: '标签',
@@ -40,17 +40,17 @@ module.exports = {
     footer: {
       contact: [
         {
-          type: 'github',
-          link: 'https://github.com/cllemon',
+          type: "github",
+          link: "https://github.com/cllemon",
         },
         {
-          type: 'mail',
-          link: 'mailto:hnx.lemon@gmail.com',
+          type: "mail",
+          link: "mailto:hnx.lemon@gmail.com",
         },
       ],
       copyright: [
         {
-          text: 'Powered by VuePress | Wooden Kite © 2017 - Present ',
+          text: "Powered by VuePress | Wooden Kite © 2017 - Present ",
         },
       ],
     },
@@ -58,16 +58,16 @@ module.exports = {
       lengthPerPage: 20,
     },
     sitemap: {
-      hostname: "https://cllemon.github.io/ii/"
+      hostname: "https://cllemon.github.io/ii/",
     },
-    comment: {
-      service: "vssue",
-      autoCreateIssue: true,
-      prefix: "[Post]",
-      owner: "cllemon",
-      repo: "ii",
-      clientId: "77ee671f2494abd9505e",
-      clientSecret: "39104cb3d8856b8cf5c4a344661729607eff715b"
-    },
+    // comment: {
+    //   service: "vssue",
+    //   autoCreateIssue: true,
+    //   prefix: "[Post]",
+    //   owner: "cllemon",
+    //   repo: "ii",
+    //   clientId: "77ee671f2494abd9505e",
+    //   clientSecret: "39104cb3d8856b8cf5c4a344661729607eff715b"
+    // },
   },
-}
+};
--- blog/_programming/2021-02-02-Code Formatter.md
+++ blog/_programming/2021-02-02-Code Formatter.md
@@ -1,9 +1,38 @@
 ---
-title: 制定团队的代码规范
-summary: 论述代码规范化
+title: 制定前端团队的代码规范
+summary: 论述代码规范化重要性，业界相关工具、并针对不同类型的代码给出推荐配置
 date: 2021-02-02
 author: Wooden Kite
 location: HuaiNan.AnHui
 ---
 
-<span style="font-size:100px;"> 🚧 </span><span style="color: gray;">建设者...</span>
+## 前言
+
+随着公司业务不断发展、团队也随之不断壮大（开发人员、项目数量以及对应项目的代码量激增），势必会面临一些问题，如：团队成员代码风格不尽相同，导致内部代码风格迥异；且团队成员水准不一，提交的代码可能存在潜在风险；在版本控制方面，项目提交的日志可能会混乱，这也对项目回溯带来了不小的挑战，尤其对内部基础库建设上（完整规范详实的提交记录有利于生成项目 CHANGELOG.md）。因此，我们需要建立一些规范来尽可能的规避上述风险，使得团队良性发展。
+
+## 业界相关辅助工具
+
+- [Prettier](https://github.com/prettier/prettier) 代码格式化工具，通过解析代码并使用自己的规则重新打印代码，从而实现一致的样式，并在必要时包装代码
+- [ESLint](https://github.com/eslint/eslint) 常用于检查常见的 JavaScript 代码错误，也可以进行代码风格检查
+- [StyleLint](https://github.com/stylelint/stylelint) 强大的现代化 linter，可帮助您避免错误并在样式中强制执行约定
+
+## 代码规范配置实践
+
+以下描述了几种不同类型的项目，规范化实践，可根据项目类型食用。
+
+### JavaScript/TypeScript Library
+
+针对 JavaScript/TypeScript 基础库，的规范配置实践
+
+#### Prettier + ESlint
+
+```sh
+# 工具安装
+
+```
+
+### Vue Application
+
+### React Application
+
+## 规范化项目提交日志
